86c0a936986de5b79884483a31680faf343dfce1
renagc <rgomes-c@student.42lisboa.com>
2023-03-14 16:01:25 +0000

    done

--- sort_utils.c
+++ sort_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgomes-c <rgomes-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rgomes-c <rgomes-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 23:57:29 by rgomes-c          #+#    #+#             */
-/*   Updated: 2023/03/14 12:07:07 by rgomes-c         ###   ########.fr       */
+/*   Updated: 2023/03/14 15:30:26 by rgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,32 +67,14 @@ void	ft_push_fit_top(t_list **stack, t_list *best_friend, char *str)
 	}
 }
 
-void	ft_teste(t_list **stack, t_list *best_friend, char *str)
+void	ft_sort_last_two(t_list **stack_a, t_list **stack_b)
 {
-	if (ft_lstsize(best_friend) > (ft_lstsize(*stack) / 2))
-	{
-		while (*stack != best_friend)
-		{
-			if (str[0] == 'a')
-				rotate(stack, "ra");
-			else if (str[0] == 'b')
-				rotate(stack, "rb");
-		}
-	}
-	else
-	{
-		while (*stack != best_friend && ft_lstsize(*stack) > 2)
-		{
-			if (str[0] == 'a')
-				rotate_reverse(stack, "rra");
-			else if (str[0] == 'b')
-				rotate_reverse(stack, "rrb");
-			printf("\nstack b:");
-			ft_printf_lst(*stack);
-			printf("\nstack b, best friend:");
-			ft_printf_lst(best_friend);
-		}
-	}
+	if ((*stack_b)->value < (*stack_b)->next->value)
+		s(&(*stack_b)->value, &(*stack_b)->next->value, "sb");
+	ft_push_fit_top(stack_a, ft_best_friend(stack_a, stack_b), "a");
+	p(stack_a, stack_b, "pa");
+	ft_push_fit_top(stack_a, ft_best_friend(stack_a, stack_b), "a");
+	p(stack_a, stack_b, "pa");
 }
 
 void	ft_sort_list(t_list **stack_a, t_list **stack_b)
@@ -103,21 +85,13 @@ void	ft_sort_list(t_list **stack_a, t_list **stack_b)
 	{
 		ft_ave_to_b(stack_a, stack_b);
 		ft_sort_3(stack_a);
-		while (*stack_b && (*stack_b)->value != 11)
+		while (*stack_b && ft_lstsize(*stack_b) > 2)
 		{
 			ft_push_fit_top(stack_b, ft_get_min_cost_b(stack_a, stack_b), "b");
-			ft_printf_lst(*stack_a);
 			ft_push_fit_top(stack_a, ft_best_friend(stack_a, stack_b), "a");
 			p(stack_a, stack_b, "pa");
 		}
-		ft_printf_lst(*stack_a);
-		printf("\nstack a e:\n");
-		ft_printf_lst(*stack_a);
-		printf("stack b e:\n");
-		ft_printf_lst(*stack_b);
-		ft_teste(stack_b, ft_get_min_cost_b(stack_a, stack_b), "b");
-		
-		
+		ft_sort_last_two(stack_a, stack_b);
 		ft_push_fit_top(stack_a, ft_stack_min(stack_a), "a");
 	}
 }
